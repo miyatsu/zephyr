ad13056749283a1b463ee2a9fa19158ae5734fe0
Ding Tao <miyatsu@qq.com>
2017-09-26 20:59:34 +0800

    app: vrbox: rfid: fix: multiple tag read overflow
    When the rfid reader scan a mass tag, it will return status code
    0x03, which the following response format is not specify in the
    tech ref book.
    
    Due to hardware structure change, the total box are shrink to
    4 * 7 = 28.
    
    Signed-off-by: Ding Tao <miyatsu@qq.com>

--- samples/net/vrbox/src/rfid.c
+++ samples/net/vrbox/src/rfid.c
@@ -20,13 +20,53 @@
 
 #include <uart.h>
 
-#define RFID_CMD_BUFF_SIZE 256
+#ifdef CONFIG_APP_RFID_DEBUG
+#include <misc/printk.h>
+#endif /* CONFIG_APP_RFID_DEBUG */
 
-static uint8_t rfid_list[4*8*2][13];
+/**
+ * Received RFIDs will be stored at here.
+ *
+ * ISO18000-6C(EPC G1G2) standard defined two length of the EPC numbers,
+ * 64 bit and 96 bit. Although the length of EPC number is variable,
+ * we use 96 bit tag for this project.
+ *
+ * EPC number is NOT the same as the value in EPC sector.
+ *
+ * We use EPC number to identify one specific RFID.
+ *
+ * 96 bit EPC length is 12 byte, but the scan result of EPC-ID will contain
+ * a length value, so use 13 byte storage will be perfect.
+ *
+ * Figure illustrated as follow:
+ * --------------------------
+ * | Length |	EPC number	|
+ * --------------------------
+ * | 1 byte |	8 bytes		|
+ * --------------------------
+ *
+ * Note: The length value will always be 0x0C, we store it anyway for either
+ * debug usage and error correcting usage.
+ * */
+static uint8_t rfid_list[4*7*2][13];
 
-char rfid_list_string[4*8*2][27];
+/**
+ * RFIDs string will be stored at here.
+ *
+ * We have hex result stored at "rfid_list" above, but the upstream will not
+ * accept value like this, we convert the hex into string locally and store
+ * at here for JSON lib usage.
+ *
+ * Note: 13 byte hex convert to string will use at least (13 * 2 + 1) byte,
+ * one byte as '\0' at the end of string to end a string.
+ * */
+char rfid_list_string[4*7*2][27];
 
-static uint8_t rfid_cmd_buff[RFID_CMD_BUFF_SIZE];
+/**
+ * The format of response shows the response length will not bigger the 256 byte,
+ * it use one byte to indicate the rest of the result.
+ * */
+static uint8_t rfid_cmd_buff[256];
 
 /**
  * @brief Get rfid_list length
@@ -37,7 +77,7 @@ static uint8_t rfid_cmd_buff[RFID_CMD_BUFF_SIZE];
 uint8_t rfid_list_get_length(void)
 {
 	uint8_t i;
-	for ( i = 0; i < 4*8*2; ++i )
+	for ( i = 0; i < 4*7*2; ++i )
 	{
 		if ( 0 == rfid_list[i][0] )
 		{
@@ -54,7 +94,7 @@ uint8_t rfid_list_get_length(void)
  * */
 void rfid_list_set_empty(void)
 {
-	for ( uint8_t i = 0; i < 4*8*2; ++i )
+	for ( uint8_t i = 0; i < 4*7*2; ++i )
 	{
 		/*
 		if ( 0 == rfid_list[i][0] )
@@ -91,7 +131,7 @@ void rfid_list_to_string(void)
 	}
 
 	/* Set rest of space to empty */
-	for (; i < 4*8*2; ++i)
+	for (; i < 4*7*2; ++i)
 	{
 		rfid_list_string[i][0] = '\0';
 	}
@@ -122,25 +162,30 @@ void rfid_list_insert(uint8_t insert_position, uint8_t *buff, uint8_t n)
 	 * |    1 byte    |    N byte   |    1 byte    |    N byte   | ... |
 	 * -----------------------------------------------------------------
 	 *
-	 * Note: The "EPC Length" ONLY indicate the "EPC Value" length,
+	 * Note: The "EPC Length" always 0x0C and ONLY indicate the "EPC Value" length,
 	 *		 NOT include "EPC Length" it self.
 	 *
 	 * For Example: Assume the "EPC-1 Length" is 12, then the "EPC-1 Value"
 	 *				will be 12 byte long. According to the note above,
-	 *				The next "EPC-2 Length" will be "buff + 12 + 1".
+	 *				the next "EPC-2 Length" byte position will be "buff + 12 + 1".
 	 *
 	 * */
 	uint8_t i, j, pos;
 	uint8_t rfid_list_len = rfid_list_get_length();
 
-	/* N items wait to be added */
+	/* N item(s) wait to be added */
 	for ( i = 0, pos = 0; i < n; ++i )
 	{
+		if ( 0x0C != buff[pos] )
+		{
+			/* The length of EPC number will always 12(0x0C) byte. */
+			return ;
+		}
 		/* Search for duplicate tags */
 		for ( j = 0; j < rfid_list_len; ++j )
 		{
 			/* Use memcmp instead of strcmp, due to non NULL termination letter */
-			if ( 0 == memcmp(rfid_list[j], &buff[pos], buff[pos]) )
+			if ( 0 == memcmp(rfid_list[j], &buff[pos], buff[pos] + 1) )
 			{
 				/* Duplicate tags */
 				break;
@@ -149,7 +194,7 @@ void rfid_list_insert(uint8_t insert_position, uint8_t *buff, uint8_t n)
 
 		if ( j < rfid_list_len )
 		{
-			/* Tag alredy in list, continue insert next tag */
+			/* Tag already in list, continue insert next tag */
 			continue;
 		}
 		else
@@ -157,7 +202,7 @@ void rfid_list_insert(uint8_t insert_position, uint8_t *buff, uint8_t n)
 			/* Save new tag to list */
 
 			/**
-			 * Note: the third param need plus 1 to save
+			 * Note: the third param need plus 1 (0x0C + 1 == 13 byte) to save
 			 *		 EPC Length field into rfid_list.
 			 **/
 			memcpy(rfid_list[rfid_list_len++], &buff[pos], buff[pos] + 1);
@@ -190,7 +235,10 @@ void rfid_list_insert(uint8_t insert_position, uint8_t *buff, uint8_t n)
  *	 Data: For some command may response with data
  * CRC-16: CRC16 check value from Len to Data, append at whole response
  *
- * Note: The "Num" field indicate the number of following EPC-IDs.
+ * WARNING: The "Num" field indicate the serial number of the first EPC-ID
+ * that this response contained. That means, the number of EPC-ID of this
+ * response is NOT identify by this field but Len field. See status 0x03
+ * and 0x04 at response parsing function for more detail.
  *
  * EPC-ID format:
  *
@@ -251,15 +299,15 @@ uint16_t rfid_cmd_crc16_calculator(uint8_t *buff, uint8_t len)
  * */
 bool rfid_cmd_crc16_check(uint8_t *buff, uint8_t len)
 {
-	uint16_t crc = ( ( (uint16_t)buff[len] << 8 ) + buff[len - 1] );
-	return crc == rfid_cmd_crc16_calculator(buff, len - 2);
+	uint16_t crc = rfid_cmd_crc16_calculator(buff, len - 2);
+	return crc == (buff[len - 1] << 8) + (buff[len - 2]);
 }
 
 void rfid_send_request(uint8_t *cmd, uint8_t cmd_len)
 {
 	/**
 	 * XXX We can add CRC16 cal at here and send it to rfid reader
-	 * so the uper caller will no need to cal CRC16 them self.
+	 * so the upper caller will no need to cal CRC16 them self.
 	 * */
 	struct device *dev = device_get_binding(CONFIG_UART_STM32_PORT_3_NAME);
 	for ( uint8_t i = 0; i < cmd_len; ++i )
@@ -272,14 +320,14 @@ int8_t rfid_get_response(uint8_t *buff)
 {
 	struct device *dev = device_get_binding(CONFIG_UART_STM32_PORT_3_NAME);
 	/* Get response length first */
-	if ( NULL == buff || 0 != uart_poll_in(dev, &buff[0]) || buff[0] < 3)
+	if ( NULL == buff || 0 != uart_poll_in(dev, &buff[0]) || buff[0] < 3 )
 	{
 		/* Wrong arg ** No input on uart ** read length less than three */
 		return -1;
 	}
 
 	/* Get the rest of the response */
-	for ( uint8_t i = 0; i < buff[0]; ++i )
+	for ( uint8_t i = 1; i <= buff[0]; ++i )
 	{
 		if ( 0 != uart_poll_in(dev, &buff[i]) )
 		{
@@ -298,40 +346,87 @@ int8_t rfid_get_response(uint8_t *buff)
 
 int rfid_query(void)
 {
+	uint8_t tags_already_insert = 0;
 	uint8_t tags_insert_position = rfid_list_get_length();
 	uint8_t cmd[5] = {0x04, 0x00, 0x01, 0xDB, 0x4B};
 
+	memset(rfid_cmd_buff, 0, 256);
+
 	/* Send cmd */
 	rfid_send_request(cmd, 5);
-
 read_again:
 	/* Get response */
-	rfid_get_response(rfid_cmd_buff);
-	if ( rfid_cmd_buff[0] < 12 )
+	if ( 0 != rfid_get_response(rfid_cmd_buff) )
 	{
+		/* Read error, or no scan result */
 		return -1;
 	}
+
+	if ( rfid_cmd_buff[0] < 12 )
+	{
+		/* No scan result */
+		return -1;
+	}
+
+	/**
+	 * Status of command response stored in rfid_cmd_buff[3].
+	 *
+	 * There is four possible value it can be:
+	 *
+	 * 0x01: Command execute finished, and return execute result.
+	 * 0x02: Command execute timeout, abort execution and return execute result.
+	 * 0x03: Command execute result can NOT response with one message, there
+	 *		 will be one or more message to send.
+	 * 0x04: Command execute error with no enough space for reader to store
+	 *		 tags it read, and return execute result.
+	 *
+	 * For example: Let's assume the first response with status code 0x03,
+	 *				and its Num is 0x10. That means current response contains
+	 *				16 EPC-IDs.
+	 *				For the next response, no matter the status code with 0x01,
+	 *				0x03 or 0x04. The Num field will be at least 17(0x11).
+	 *				Because the Num field is NOT the length of current response
+	 *				EPC-IDs but the numbers of EPC-IDs transmited until current
+	 *				response arrives.
+	 * */
+
 	/**
 	 * We use switch/case rather than if/else
 	 * for error check extension consideration.
 	 * */
-	switch (rfid_cmd_buff[3])
+	switch ( rfid_cmd_buff[3] )
 	{
 		case 0x01:	/* OK */
 			/* Update readed tag list */
-			rfid_list_insert(tags_insert_position, &rfid_cmd_buff[5], rfid_cmd_buff[4]);
+			rfid_list_insert(tags_insert_position,
+							 &rfid_cmd_buff[5],
+							 rfid_cmd_buff[4] - tags_already_insert);
+			/**
+			 * Note the third parameter of this function call, we don't know if
+			 * current response is the first response we received.
+			 *
+			 * Use the whole rfid_cmd_buff[4] take away tags_already_insert, we
+			 * can get the number of EPC-IDs that this response contains.
+			 * */
 			return 0;
 
 		case 0x02:	/* Timeout */
 			return -1;
 
 		case 0x03:	/* There is more data */
+			/* Nothing, same as status code 0x04 */
+
 		case 0x04:	/* Too many tags, not read all tags yet */
 			/* Update readed tag list */
-			rfid_list_insert(tags_insert_position, &rfid_cmd_buff[5], rfid_cmd_buff[4]);
+			rfid_list_insert(tags_insert_position,
+							 &rfid_cmd_buff[5],
+							 rfid_cmd_buff[4] - tags_already_insert);
 
-			/* Adjust insert position, to avoid new data overwriten */
+			/* Adjust insert position, to avoid new data overwritten */
 			tags_insert_position = rfid_list_get_length();
+
+			/* Update insert numbers */
+			tags_already_insert += rfid_cmd_buff[4];
 
 			/* Re-read tags, and drop duplicate tags */
 			goto read_again;
@@ -348,7 +443,11 @@ void rfid_scan(uint8_t times)
 	while ( times-- )
 	{
 		rfid_query();
-		k_sleep(3);		/* 3ms will be fine, 2ms we will not get packages*/
+		void rfid_cmd_print_head(void);
+		void rfid_list_print(void);
+		rfid_cmd_print_head();
+		//rfid_list_print();
+		k_sleep(200);		/* 3ms will be fine, 2ms we will not get packages*/
 	}
 	rfid_list_to_string();
 }
@@ -369,11 +468,22 @@ void rfid_list_print(void)
 
 void rfid_cmd_print(void)
 {
+	printk("rfid_cmd_buff:\n");
 	for ( uint8_t i = 0; i < rfid_cmd_buff[0] + 1; ++i )
 	{
-		printk("%X ", rfid_cmd_buff[i]);
+		printk("%02X ", rfid_cmd_buff[i]);
 	}
-	printk("\n");
+	printk("\n\n");
+}
+
+void rfid_cmd_print_head(void)
+{
+	printk("response head:");
+	for ( uint8_t i = 0; i < 4; ++i )
+	{
+		printk("%02X ", rfid_cmd_buff[i]);
+	}
+	printk("%d\n", rfid_cmd_buff[4]);
 }
 
 void rfid_list_string_print(void)
@@ -389,11 +499,6 @@ int rfid_test(void)
 	while (1)
 	{
 		rfid_scan(10);
-		printk("query done!\n");
-
-		printk("start to print rfid_list_string...\n");
-		rfid_list_string_print();
-		k_sleep(200);
 	}
 	return 0;
 }
--- samples/net/vrbox/src/rfid.h
+++ samples/net/vrbox/src/rfid.h
@@ -1,7 +1,7 @@
 #ifndef RFID_H
 #define RFID_H
 
-extern char rfid_list_string[4*8*2][27];
+extern char rfid_list_string[4*7*2][27];
 
 int rfid_init(void);
 void rfid_scan(uint8_t times);
