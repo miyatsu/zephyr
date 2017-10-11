3370d704446f9e2354bdf8fd6ff7f2be9439e532
Ding Tao <miyatsu@qq.com>
2017-10-11 14:46:38 +0800

    app: vrbox: main stepper motor bug fix
    Fix some main stepper motor bugs
    
    Test passed, GPIO table is not checked
    
    Signed-off-by: Ding Tao <miyatsu@qq.com>

--- samples/net/vrbox/src/axle.c
+++ samples/net/vrbox/src/axle.c
@@ -21,9 +21,60 @@
 #include <misc/printk.h>
 #endif /* CONFIG_APP_AXLE_DEBUG */
 
-#include "axle.h"
+#include "gpio_comm.h"
+#include "config.h"
 
-static uint8_t axle_position;
+/**
+ * P4:
+ * -----------------------
+ * | PB9    1 |  2  PB8	 |	Not suggest using PA8 as GPIO
+ * | PB7    3 |  4  PB6	 |	Check
+ * | PB5    5 |  6  PB4  |	Check
+ * | PB3    7 |  8  PG15 |
+ * | PG14   9 | 10  PG13 |	ETH
+ * | PG12  11 | 12  PG11 |	PG11 ETH
+ * | PG10  13 | 14  PG9  |
+ * | PD7   15 | 16  PD6  |
+ * | PD5   17 | 18  PD4  |
+ * | PD3   19 | 20  PD2  |
+ * | PC12  21 | 22  PC11 |
+ * | PC10  23 | 24  PA15 |
+ * | PA14  25 | 26  PA13 |
+ * | PA8   27 | 28  PC9  |
+ * | PC8   29 | 30  PC7  |
+ * | PC6   31 | 32  PG8  |
+ * | PG7   33 | 34  PG6  |
+ * | PG5   35 | 36  PG4  |
+ * | PG3   37 | 38  PG2  |
+ * | PD13  39 | 40  PD12 |
+ * | PD11  41 | 42  PB15 |
+ * | PB14  43 | 44  GND  |
+ * -----------------------
+ *
+ * */
+
+/**
+ * Current axle position, default NONE
+ * initialized by function axle_ini()
+ *
+ * This variable MUST initialize before use any axle APIs
+ * */
+static uint8_t axle_position = 0;
+
+static struct gpio_group_pin_t axle_gpio_table[] =
+{
+	{GPIO_GROUP_D, 14},		/* Axle position start*/
+	{GPIO_GROUP_D, 15},
+	{GPIO_GROUP_D,  0},
+	{GPIO_GROUP_D,  1},
+	{GPIO_GROUP_E,  7},
+	{GPIO_GROUP_E,  8},
+	{GPIO_GROUP_E,  9},		/* Axle position end */
+
+	{GPIO_GROUP_B,  3},		/* PWM output */
+	{GPIO_GROUP_E, 10},		/* Axle rotate direction */
+	{GPIO_GROUP_E, 11},		/* Stepper motor break */
+};
 
 /**
  * @brief Read position index's level.
@@ -31,60 +82,18 @@ static uint8_t axle_position;
  * High level means current axle is at @param index position
  * Low level means current axle is not at @param index position
  *
- * @param index Position that the gpio reside, value of [1-7]
+ * @param index Position that the gpio reside, value of [0-6]
  *
  * @return	0	low level (Axle current not at @index position)
  *			1	hight level (Axle current at @index position)
  * */
-bool axle_position_read_gpio(uint8_t index)
+static bool axle_position_read_gpio(uint8_t index)
 {
-	/**
-	 *
-	 * The GPIO pin number of axle position detector, we designed as follow:
-	 *
-	 * ----------------------------------
-	 * | axle position | GPIO pin number|
-	 * ----------------------------------
-	 * |	A1				PB.15		|
-	 * |	A2				PD.11		|
-	 * |	A3				PD.12		|
-	 * |	A4				PD.13		|
-	 * |	A5				PG.2		|
-	 * |	A6				PG.3		|
-	 * |	A7				PG.4		|
-	 * ----------------------------------
-	 *
-	 * All those GPIO pins are at the top of the left pin row.
-	 *
-	 * */
+	uint32_t val = 0;
 
-	/* Note: the first item of this table is reserved, DO NOT use it. */
-	static const uint8_t axle_gpio_map_table[] =
-	{
-		0, 15, 11, 12, 13, 2, 3, 4
-	};
-	struct device *dev = NULL;
-	uint32_t value_read_from_gpio = 0;
+	gpio_comm_read(&axle_gpio_table[index], &val);
 
-	/* Using extended GNU C sytax */
-	switch ( index )
-	{
-		case 1:
-			dev = device_get_binding("GPIOB");
-			break;
-		case 2 ... 4:
-			dev = device_get_binding("GPIOD");
-			break;
-		case 5 ... 7:
-			dev = device_get_binding("GPIOG");
-			break;
-		default:
-			return -1;
-	}
-
-	gpio_pin_read(dev, axle_gpio_map_table[index], &value_read_from_gpio);
-
-	return value_read_from_gpio;
+	return val;
 }
 
 /**
@@ -93,10 +102,10 @@ bool axle_position_read_gpio(uint8_t index)
  * @return	The axle current position, 1-7 is expected.
  *			return 0 if can not read the axle position,
  *			or some error happened during read the position,
- *			or axle are rotating.
- *
+ *			or axle are rotating when request the current
+ *			axle position.
  * */
-uint8_t axle_position_read(void)
+static uint8_t axle_position_read(void)
 {
 	/* Axle position can ONLY at one place, possible value are placed as follow */
 	const static uint8_t axle_position_table[] =
@@ -111,7 +120,7 @@ uint8_t axle_position_read(void)
 	for ( i = 0; i < 7; ++i )
 	{
 		/* Read all status of axle position, in case of two position is valid. */
-		axle_position_bit = axle_position_read_gpio(i) ? 1 : 0;
+		axle_position_bit = axle_position_read_gpio(i) ? 0 : 1;
 
 		axle_position_bit_map |= ( axle_position_bit << i );
 	}
@@ -119,6 +128,7 @@ uint8_t axle_position_read(void)
 	/* Lookup all possibile value table */
 	for ( i = 0; i < 7; ++i )
 	{
+		/* Position match the possibile position */
 		if ( axle_position_bit_map == axle_position_table[i] )
 		{
 			return i + 1;
@@ -127,61 +137,77 @@ uint8_t axle_position_read(void)
 	return 0;
 }
 
-
-/**
- *
- * Axle rotate pin number, we designed as follow:
- *
- * ------------------------------------
- * | axle_function  | GPIO pin number |
- * ------------------------------------
- * | axle_lock				PG.6	  |
- * | axle_direction			PG.7	  |
- * | axle_enable			PG.8	  |
- * ------------------------------------
- *
- * */
-
 /**
  * @brief Set axle break, to control axle free rotate or lock it
  *
- * @param (AXLE_ROTATE_LOCK = 0)/(AXLE_ROTATE_UNLOCK = 1)
+ * @param unlock 1 unlock
+ *			   0 lock
  * */
-void axle_set_rotate_lock_unlock(enum axle_rotate_lock_unlock lock_unlock)
+static void axle_set_rotate_lock_unlock(uint8_t unlock)
 {
-	struct device *dev = device_get_binding("GPIOG");
-	gpio_pin_write(dev, 6, lock_unlock);
+	gpio_comm_write(&axle_gpio_table[9], !unlock);
 }
 
 /**
  * @brief Set axle rotate direction
  *
- * @param (AXLE_ROTATE_LEFT)/(AXLE_ROTATE_RIGHT)
+ * @param direction 1 clockwise
+ *					0 anticlockwise
  * */
-void axle_set_rotate_direction(enum axle_rotate_direction direction)
+static void axle_set_rotate_direction(uint8_t direction)
 {
-	struct device *dev = device_get_binding("GPIOG");
-	gpio_pin_write(dev, 7, direction);
+	gpio_comm_write(&axle_gpio_table[8], direction);
 }
 
 /**
  * @brief Enable or Disable axle rotate
  *
- * @param (AXLE_ROTATE_DISABLE = 0)/(AXLE_ROTATE_ENABLE = 1)
+ * @param enable 1 enable
+ *				 0 disable
+ * @return true Set Enable/Disable success
+ * 		   false Otherwise
  * */
-void axle_set_rotate_on_off(enum axle_rotate_on_off on_off)
+static bool axle_set_rotate_enable_disable(uint8_t enable)
 {
-	struct device *dev = device_get_binding("GPIOG");
-	gpio_pin_write(dev, 8, on_off);
+	struct device *pwm_dev = device_get_binding(CONFIG_APP_AXLE_PWM_DEV_NAME);
+
+	int8_t   rc;
+	uint8_t  channel = CONFIG_APP_AXLE_PWM_OUTPUT_CHANNEL;
+	uint32_t period = CONFIG_APP_AXLE_PWM_PERIOD;
+	uint32_t pulse_width = period/2;
+
+	if ( enable )
+	{
+		/* Unlock the break */
+		axle_set_rotate_lock_unlock(1);
+
+		/* Wait the break fully unlocked */
+		k_sleep(50);
+
+		/* Start to rotate */
+		rc = pwm_pin_set_usec(pwm_dev, channel, period, pulse_width);
+	}
+	else
+	{
+		/* Stop rotate */
+		rc = pwm_pin_set_usec(pwm_dev, channel, period, 0);
+
+		/* Wait the axle fully stoped */
+		k_sleep(50);
+
+		/* Lock the break */
+		axle_set_rotate_lock_unlock(0);
+	}
+	return !rc;
 }
 
 
 /**
  * --------------------
  * |       Center     |
- * |    (A7)   (A1)   |
- * |  (A6)       (A2) |
- * |    (A5)   (A3)   |
+ * |    (A1)   (A7)   |
+ * |  (A2)       (A6) |
+ * |    (A3)   (A5)   |
  * |		(A4)      |
  * --------------------
  *  Figure: Axle
@@ -192,103 +218,40 @@ void axle_set_rotate_on_off(enum axle_rotate_on_off on_off)
  *     or anticlockwise for 195 degrees.
  * (1).One circle has 7 boxes, each box at a distance of 360/7 degrees.
  * (2).We assume A4 is at the start position, and make the axle can either rotate
- *     clockwise to A1 or rotate anticlockwise to A7 without reach the limitation
+ *     clockwise to A7 or rotate anticlockwise to A1 without reach the limitation
  *     of rotate angles.
  *
  *
- * Note: A1 is the start position of the axle.
- * (a). The max angle rotate to right is A1 to A4.
- * (b). The max angle rotate to left is A1 to A8.
+ * Note: A4 is the start position of the axle.
+ * (a). The max angle rotate clockwise is A4 to A7.
+ * (b). The max angle rotate anticlockwise is A4 to A1.
  *
- * For example: (1). If we at A4 and want goto A5, MUST rotate left to A5
- *					all the way pass though A3, A2, A1.
- *				(2). If we at A4 and want goto A8, MUST rotate left to A8
- *					all the way pass though A3, A2, A1, A5, A6, A7.
+ * For example: (1). If we at A3 and want goto A6, MUST rotate clockwise to A6
+ *					all the way pass though A4, A5.
+ *				(2). If we at A7 and want goto A2, MUST rotate anticlockwise to
+ *					A2 all the way pass though A6, A5, A4, A3.
  *
- * Due to this special hardware design, In order to simplify the software design,
+ * Due to this special hardware design, in order to simplify the software design,
  * we make a spwcial table for rotate operation.
  *
  * Row is the current axle position, and the column is the destination axle position.
  *
- * Positive means axle should rotate right N times.
- * Negetive means axle should rotate left N times.
+ * Positive means axle should rotate clockwise N times.
+ * Negetive means axle should rotate anticlockwise N times.
  *
- * -----------------------------------------------
- * |   |  1  |  2  |  3  |  4  |  5  |  6  |  7  |
- * -----------------------------------------------
- * | 1 |  0  | -1  | -2  | -3  | -4  | -5  | -6  |
- * | 2 |  1  |  0  | -1  | -2  | -3  | -4  | -5  |
- * | 3 |  2  |  1  |  0  | -1  | -2  | -3  | -4  |
- * | 4 |  3  |  2  |  1  |  0  | -1  | -2  | -3  |
- * | 5 |  4  |  3  |  2  |  1  |  0  | -1  | -2  |
- * | 6 |  5  |  4  |  3  |  2  |  1  |  0  | -1  |
- * | 7 |  6  |  5  |  4  |  3  |  2  |  1  |  0  |
- * -----------------------------------------------
+ * ------------------------------------------------
+ * |   ||  1  |  2  |  3  |  4  |  5  |  6  |  7  |
+ * ------------------------------------------------
+ * | 1 ||  0  | -1  | -2  | -3  | -4  | -5  | -6  |
+ * | 2 ||  1  |  0  | -1  | -2  | -3  | -4  | -5  |
+ * | 3 ||  2  |  1  |  0  | -1  | -2  | -3  | -4  |
+ * | 4 ||  3  |  2  |  1  |  0  | -1  | -2  | -3  |
+ * | 5 ||  4  |  3  |  2  |  1  |  0  | -1  | -2  |
+ * | 6 ||  5  |  4  |  3  |  2  |  1  |  0  | -1  |
+ * | 7 ||  6  |  5  |  4  |  3  |  2  |  1  |  0  |
+ * ------------------------------------------------
  *
  * */
-
-/**
- * @brief Rotate axle 360/7 degree with direction of @direction
- *
- * @param direction The direction the the axle should rotate
- *
- * @return 0, rotate success
- *		   -1, rotate error
- * */
-int8_t axle_rotate(enum axle_rotate_direction direction)
-{
-	uint8_t wait_time;
-
-	/* Set direction signal */
-	axle_set_rotate_direction(direction);
-
-	/* Unlock axle */
-	axle_set_rotate_lock_unlock(AXLE_ROTATE_UNLOCK);
-
-	/* Enable axle */
-	axle_set_rotate_on_off(AXLE_ROTATE_ENABLE);
-
-	/**
-	 * TODO XXX FIXME
-	 * 
-	 * Create new thread to check position ?
-	 * Using k_queue ?
-	 * Pending to design...
-	 *
-	 * */
-	/* Start to rotate, 45 degrees we will stop the axle */
-	/* We need a little bit time to wait axle in position */
-	k_sleep(1000);		/* WARNING: This period of time depending on PWM frequency */
-
-	for ( wait_time = 0; wait_time < 100; wait_time += 10 )
-	{
-		/* Axle in position */
-		if ( 0 != axle_position_read() )
-		{
-			break;
-		}
-
-		/* Axle is not in position, wait for more 10ms */
-		k_sleep(10);
-	}
-
-	/* Disable axle */
-	axle_set_rotate_on_off(AXLE_ROTATE_DISABLE);
-
-	/* Lock axle in case of free rotate */
-	axle_set_rotate_lock_unlock(AXLE_ROTATE_LOCK);
-
-	/* Update axle position */
-	axle_position = axle_position_read();
-
-	/* Wait axle rotate in position timeout */
-	if ( wait_time >= 100 )
-	{
-		return -1;
-	}
-
-	return 0;
-}
 
 /**
  * @brief Rotate the axle to particular position
@@ -300,7 +263,13 @@ int8_t axle_rotate(enum axle_rotate_direction direction)
  * */
 int8_t axle_rotate_to(uint8_t destination_position)
 {
-	/* Update current axle position */
+	uint8_t	axle_posision;
+	uint8_t	axle_rotate_direction;
+	int8_t	axle_rotate_times;
+
+	uint16_t i, wait_time_in_sec = CONFIG_APP_AXLE_ROTATE_TIMEOUT_IN_SEC;
+
+	/* Get current axle position */
 	axle_position = axle_position_read();
 
 	/* Boundary check */
@@ -310,80 +279,187 @@ int8_t axle_rotate_to(uint8_t destination_position)
 		return -1;
 	}
 
-	int8_t rotate_times = (int8_t)axle_position - (int8_t)destination_position;
+	/**
+	 * When we assume the A4 is the center of the axle, either rotate clockwise
+	 * or anticlockwise, the axle both need rotate three times to get the limit
+	 * position.
+	 *
+	 * By analyzing the mathematics of axle rotate pattern, we find that: move
+	 * axle from N position to M position, the rotate times will be |N-M|, the
+	 * rotate direction depending on N and M.
+	 * */
+	axle_rotate_times = (int8_t)axle_position - (int8_t)destination_position;
+
+	/* Positive rotate clockwise, Negetive rotate anticlockwise */
+	axle_set_rotate_direction(axle_rotate_times > 0 ? 1 : 0);
+
+	/* Get ride of signed, since we already set rotate direction */
+	axle_rotate_times = axle_rotate_times >= 0 ? axle_rotate_times : -axle_rotate_times;
 
 	/* Already at requested position */
-	if ( 0 == rotate_times )
+	if ( 0 == axle_rotate_times )
 	{
 		return 0;
 	}
 
-	if ( rotate_times > 0 )
+	/* Start to rotate axle */
+	axle_set_rotate_enable_disable(1);
+
+	for ( i = 0; i < ( wait_time_in_sec * 10 ) * axle_rotate_times; ++i )
 	{
-		for ( uint8_t times = 0; times < rotate_times; ++times )
+		/**
+		 * Polling axle position, check if it is reached the destination
+		 * position
+		 * */
+		if ( destination_position == axle_position_read() )
 		{
-			if ( 0 != axle_rotate(AXLE_ROTATE_CLOCKWISE) )
-			{
-				return -1;
-			}
+			break;
 		}
-	}
-	else
-	{
-		for ( uint8_t times = 0; times < -rotate_times; ++times )
-		{
-			if (0 != axle_rotate(AXLE_ROTATE_ANTICLOCKWISE) )
-			{
-				return -1;
-			}
-		}
+		k_sleep(100);
 	}
 
-	return 0;
-}
+	/* Stop rotate axle */
+	axle_set_rotate_enable_disable(0);
 
-int8_t axle_self_check(void)
-{
-	axle_position = axle_position_read();
-	if ( 0 == axle_position )
+	/* Axle rotate timeout */
+	if ( i >= ( wait_time_in_sec * 10 ) * axle_rotate_times )
 	{
-		/* Axle position sensor error */
 		return -1;
 	}
 
-	/* Rotate to most left position */
-	for ( uint8_t position = 8; position >= 1; --position )
-	{
-		if ( 0 != axle_rotate_to(position) )
-		{
-			return -1;
-		}
-	}
-
-	/* Rotate to most right position, and leave it for door check */
-	for ( uint8_t position = 1; position <= 8; ++position )
-	{
-		if ( 0 != axle_rotate_to(position) )
-		{
-			return -1;
-		}
-	}
-
+	/* Axle rotate to correct position */
 	return 0;
 }
 
-
-int axle_init(void)
+/**
+ * @brief Rotate axle 360/7 degree with direction of @direction
+ *
+ * @param direction The direction the the axle should rotate
+ *					1 rotate clockwise
+ *					0 rotate anticlockwise
+ *
+ * @return  0, rotate success
+ *		   -1, rotate error
+ * */
+static int8_t axle_rotate_init(uint8_t direction)
 {
-	/* TODO: Wait for other code done, PWM output is needed. */
+	uint16_t i, wait_time_in_sec = CONFIG_APP_AXLE_ROTATE_TIMEOUT_IN_SEC;
+
+	/* Set direction signal */
+	axle_set_rotate_direction(direction);
+
+	/* Enable axle */
+	axle_set_rotate_enable_disable(1);
+
+	for ( i = 0; i < wait_time_in_sec * 10; ++i )
+	{
+		/* Axle is in position */
+		if ( 0 != axle_position_read() )
+		{
+			break;
+		}
+
+		/* Axle is not in position, wait for more 100ms */
+		k_sleep(100);
+	}
+
+	/* Disable axle */
+	axle_set_rotate_enable_disable(0);
+
+	/* Update axle position */
+	axle_position = axle_position_read();
+
+	/* Check for timeout */
+	if ( i >= wait_time_in_sec )
+	{
+		/* Wait axle rotate to next position timeout */
+		return -1;
+	}
+
 	return 0;
 }
+
+/**
+ * @brief Axle initial function
+ *		  Rotate the axle to place that in position
+ *
+ * @return  0 OK
+ *		   -1 Failed
+ * */
+int8_t axle_init(void)
+{
+	uint8_t i;
+	uint32_t temp;
+
+	/* Flush the axle position gpio input */
+	for ( i = 0; i < 7; ++i )
+	{
+		gpio_comm_read(&axle_gpio_table[i], &temp);
+	}
+
+	axle_set_rotate_direction(0);		/* Flush output and set rotate anticlockwise */
+	axle_set_rotate_lock_unlock(0);		/* Flush output and set the break locked */
+
+	/**
+	 * Rotate the axle to get make the axle at one certain position
+	 *
+	 * If rotate anticlockwise may reach the limitation of rotate
+	 * angles, then try to rotate clockwise
+	 * */
+	return ( 0 == axle_rotate_init(0) ) || ( 0 == axle_rotate_init(1) ) ? 0 : -1;
+}
+
+#ifdef CONFIG_APP_AXLE_FACTORY_TEST
+
+int8_t axle_factory_test(void)
+{
+	return 0;
+}
+
+#endif /* CONFIG_APP_AXLE_FACTORY_TEST */
 
 #ifdef CONFIG_APP_AXLE_DEBUG
+
+void axle_debug_1(void)
+{
+	/* Unlock break */
+	axle_set_rotate_lock_unlock(0);
+	while (1)
+	{
+		if ( axle_set_rotate_enable_disable(1) )
+		{
+			printk("Set PWM OK\n");
+		}
+		k_sleep(3000);
+		if ( axle_set_rotate_enable_disable(0) )
+		{
+			printk("Set PWM OK\n");
+		}
+		k_sleep(3000);
+		printk("Done!\n");
+	}
+}
+
+void axle_debug(void)
+{
+	axle_init();
+	printk("Position confirmed\n");
+	while (1)
+	{
+		for ( uint8_t i = 1; i <= 7; ++i )
+		{
+			printk("Rotate to %d\n", i);
+			axle_rotate_to(i);
+			k_sleep(3000);
+		}
+	}
+	return ;
+}
+
 /**
  * PA2 TIM2_CH3
  * */
-void axle_debug(void)
+void axle_debug_pwm(void)
 {
 	/**
 	 * WARNING: The Enable/Disable Input on stepper motor driver is NOT working!!!
@@ -395,6 +471,8 @@ void axle_debug(void)
 	 * */
 	printk("Endtering %s() ...\n", __func__);
 	struct device *dev = device_get_binding(CONFIG_PWM_STM32_2_DEV_NAME);
+	axle_set_rotate_lock_unlock(0);
+	k_sleep(100);
 	if ( pwm_pin_set_usec(dev, 2, 5000, 2500) )
 	{
 		printk("pwm pin set failes!\n");
@@ -403,7 +481,8 @@ void axle_debug(void)
 	{
 		k_sleep(1000);
 	}
-	return ;
+	return 0;
 }
+
 #endif /* CONFIG_APP_AXLE_DEBUG */
 
--- samples/net/vrbox/src/axle.h
+++ samples/net/vrbox/src/axle.h
@@ -1,23 +1,41 @@
 #ifndef AXLE_H
 #define AXLE_H
 
-enum axle_rotate_lock_unlock
-{
-	AXLE_ROTATE_LOCK = 0,
-	AXLE_ROTATE_UNLOCK = 1
-};
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
 
-enum axle_rotate_direction
-{
-	AXLE_ROTATE_CLOCKWISE,
-	AXLE_ROTATE_ANTICLOCKWISE
-};
+#include <stdint.h>
 
-enum axle_rotate_on_off
-{
-	AXLE_ROTATE_DISABLE = 0,
-	AXLE_ROTATE_ENABLE = 1
-};
+/**
+ * @brief Axle hardware initializer
+ *
+ * @return  0 Succeess
+ *		   <0 Some error happened
+ * */
+int8_t axle_init(void);
 
+/**
+ * @brief Rotate the axle to particular position
+ *
+ * @param destination_position
+ *		  The real position we want to go, value [1-7] is expected.
+ * @return  0 Success
+ *		   <0 Some error happened
+ * */
+int8_t axle_rotate_to(uint8_t destination_position);
+
+#ifdef CONFIG_APP_AXLE_FACTORY_TEST
+int8_t axle_factory_test(void);
+#endif /* CONFIG_APP_AXLE_FACTORY_TEST */
+
+#ifdef CONFIG_APP_AXLE_DEBUG
+void axle_debug(void);
+#endif /* CONFIG_APP_AXLE_DEBUG */
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
 
 #endif /* AXLE_H */
+
