db25286055c4261588ec973fac3a5e73748f3deb
Ding Tao <miyatsu@qq.com>
2017-10-20 19:19:59 +0800

    app: vrbox: gpio: make configure separated as a function
    Add gpio_comm_conf() for wrapper of gpio_pin_configure().
    
    Add API usage comments.
    
    Add Copyright comments.
    
    Signed-off-by: Ding Tao <miyatsu@qq.com>

--- samples/net/vrbox/src/gpio_comm.c
+++ samples/net/vrbox/src/gpio_comm.c
@@ -1,23 +1,61 @@
+/**
+ * Copyright (c) 2017 Shenzhen Trylong Intelligence Technology Co., Ltd. All rights reserved.
+ *
+ * @file gpio_comm.c
+ *
+ * @brief GPIO operation wrapper functions
+ *
+ * @author Ding Tao <miyatsu@qq.com>
+ *
+ * @date 17:43:07 October 20, 2017 GTM+8
+ *
+ * */
 #include <stdint.h>
 
 #include "gpio_comm.h"
 
+/* GPIO name table */
 const char *gpio_group_dev_name_table[] =
 {
 	"GPIOA", "GPIOB", "GPIOC", "GPIOD", "GPIOE", "GPIOF", "GPIOG"
 };
 
+/**
+ * @brief Wrapper function of gpio_pin_read using struct gpio_group_pin_t
+ *
+ * @param gpio Pin define, type of struct gpio_group_pin_t
+ *
+ * @param value_addr Read out value will save to this address
+ * */
 void gpio_comm_read(struct gpio_group_pin_t *gpio, uint32_t *value_addr)
 {
 	struct device *dev = device_get_binding(gpio_group_dev_name_table[gpio->gpio_group]);
-	gpio_pin_configure(dev, gpio->gpio_pin, GPIO_DIR_IN | GPIO_PUD_PULL_UP);
 	gpio_pin_read(dev, gpio->gpio_pin, value_addr);
 }
 
+/**
+ * @brief Wrapper function of gpio_pin_write using struct gpio_group_pin_t
+ *
+ * @param gpio Pin define, type of struct gpio_group_pin_t
+ *
+ * @param value Write out value
+ * */
 void gpio_comm_write(struct gpio_group_pin_t *gpio, uint32_t value)
 {
 	struct device *dev = device_get_binding(gpio_group_dev_name_table[gpio->gpio_group]);
-	gpio_pin_configure(dev, gpio->gpio_pin, GPIO_DIR_OUT | GPIO_PUD_PULL_UP);
 	gpio_pin_write(dev, gpio->gpio_pin, value);
+}
+
+/**
+ * @brief Wrapper function of gpio_pin_configure using struct gpio_group_pin_t
+ *
+ * @param gpio Pin define, type of struct gpio_group_pin_t
+ *
+ * @param flag GPIO config flag, defined in file gpio.h
+ * */
+void gpio_comm_conf(struct gpio_group_pin_t *gpio, uint32_t flag)
+{
+	struct device *dev = device_get_binding(gpio_group_dev_name_table[gpio->gpio_group]);
+	gpio_pin_configure(dev, gpio->gpio_pin, flag);
 }
 
--- samples/net/vrbox/src/gpio_comm.h
+++ samples/net/vrbox/src/gpio_comm.h
@@ -1,3 +1,13 @@
+/**
+ * Copyright (c) 2017 Shenzhen Trylong Intelligence Technology Co., Ltd. All rights reserved.
+ *
+ * @file GPIO wrapper function API
+ *
+ * @author Ding Tao <miyatsu@qq.com>
+ *
+ * @date 17:45:05 October 20, 2017 GTM+8
+ *
+ * */
 #ifndef GPIO_COMM_H
 #define GPIO_COMM_H
 
@@ -26,11 +36,36 @@ struct gpio_group_pin_t
 
 extern const char *gpio_group_dev_name_table[];
 
+/**
+ * @brief Wrapper function of gpio_pin_read using struct gpio_group_pin_t
+ *
+ * @param gpio Pin define, type of struct gpio_group_pin_t
+ *
+ * @param value_addr Read out value will save to this address
+ * */
 void gpio_comm_read(struct gpio_group_pin_t *gpio, uint32_t *value_addr);
+
+/**
+ * @brief Wrapper function of gpio_pin_write using struct gpio_group_pin_t
+ *
+ * @param gpio Pin define, type of struct gpio_group_pin_t
+ *
+ * @param value Write out value
+ * */
 void gpio_comm_write(struct gpio_group_pin_t *gpio, uint32_t value);
+
+/**
+ * @brief Wrapper function of gpio_pin_configure using struct gpio_group_pin_t
+ *
+ * @param gpio Pin define, type of struct gpio_group_pin_t
+ *
+ * @param flag GPIO config flag, defined in file gpio.h
+ * */
+void gpio_comm_conf(struct gpio_group_pin_t *gpio, uint32_t flag);
 
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
 
 #endif /* GPIO_COMM_H */
+
