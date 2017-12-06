071e26de342b8871789b4f9514c235d9face3984
Ding Tao <miyatsu@qq.com>
2017-12-06 20:23:11 +0800

    app: vrbox: dfu: Change http API to net_app
    The old HTTP api is deprecated, we change the HTTP api to newest http
    network stack.
    
    The request processing is successfuly tested on STM32F407 SoC, but the
    response callback funtion is still old. We need be sure that the
    response function is also change to the newest HTTP api.
    
    Signed-off-by: Ding Tao <miyatsu@qq.com>

--- samples/net/vrbox/src/dfu_http.c
+++ samples/net/vrbox/src/dfu_http.c
@@ -1,34 +1,76 @@
+/**
+ * Copyright (c) 2017 Shenzhen Trylong Intelligence Technology Co., Ltd. All rights reserved.
+ *
+ * @file dfu_http.c
+ *
+ * @brief Download the firmware via HTTP by a single url
+ *
+ * @author Ding Tao <miyatsu@qq.com>
+ *
+ * @date 15:02:41 December 6, 2017 GTM+8
+ *
+ * */
 #include <stdint.h>
 #include <errno.h>
 
-#include <http_parser.h>
-#include <http.h>
+#include <net/http_parser.h>
+#include <net/http.h>
 
 #include <dfu/flash_img.h>
 
+#include "net_app_buff.h"
 #include "config.h"
 
-struct http_response_user_data
+/**
+ * The net_core.h alredy defined SYS_LOG_DOMAIN, this net_core.h will be
+ * include by all net_app related network stack head files include http.h.
+ * We want use the orignal SYS_LOG_*, so undefine it
+ * */
+#undef SYS_LOG_DOMAIN
+#undef SYS_LOG_LEVEL
+
+#define SYS_LOG_DOMAIN "dfu_http"
+#ifdef CONFIG_APP_DFU_HTTP_DEBUG
+	#define SYS_LOG_LEVEL	SYS_LOG_LEVEL_DEBUG
+#else
+	#define SYS_LOG_LEVEL	SYS_LOG_LEVEL_ERROR
+#endif /* CONFIG_APP_DFU_HTTP_DEBUG */
+#include <logging/sys_log.h>
+
+struct http_user_data
 {
-	int		rc_flash_img;
-	int		rc_http_parse;
-	k_sem	sem;
+	struct flash_img_context *	flash_img_ctx;
+	int							rc_flash_img;
+	int							rc_http_parse;
+	struct k_sem				sem;
 };
 
-static void http_response_cb(struct http_client_ctx *http_ctx, 
+static void http_response_cb(struct http_ctx *http_ctx, 
 		uint8_t *data, size_t buf_len, size_t data_len,
 		enum http_final_call data_end, void *user_data_arg)
 {
-	struct http_response_user_data *http_user_data = user_data_arg;
-
+	int			rc;
+	printk("Message comming: body_start = %d\n", http_ctx->http.rsp.body_start?1:0);
+	for ( int i = 0; i < data_len; ++i )
+	{
+		printk("%c", data[i]);
+	}
+	printk("\n");
+	return ;
+	/* Use to mark the body position */
 	uint8_t *	body_start;
 	size_t		body_len;
 
+	/* User data */
+	struct http_user_data *http_user_data;
+
+	http_user_data = (struct http_user_data *)user_data_arg;
+
 	/* Mark the body position */
-	if ( NULL != ctx->rsp.body_start )
+	if ( NULL != http_ctx->http.rsp.body_start )
 	{
 		/* This fragment contains http headers */
-		body_start	= ctx->rsp.body_start;
+		body_start	= http_ctx->http.rsp.body_start;
 		body_len	= data_len - ( body_start - data );
 
 		/**
@@ -41,7 +83,7 @@ static void http_response_cb(struct http_client_ctx *http_ctx,
 		 * not contains the http headers. So we just move the
 		 * body_start to the very beginning of the recv buffer.
 		 * */
-		ctx->rsp.body_start = data;
+		//http_ctx->rsp.body_start = data;
 	}
 	else
 	{
@@ -51,18 +93,18 @@ static void http_response_cb(struct http_client_ctx *http_ctx,
 
 	rc = flash_img_buffered_write(http_user_data->flash_img_ctx,
 					body_start, body_len, data_end);
-	if ( 0 != rc )
+	if ( 0 != rc && 0 == http_user_data->rc_flash_img )
 	{
 		/**
 		 * Flash write error handle
 		 *
 		 * Do not exit, wait http transfer complete
 		 * */
-		http_user_data->rc_flash_img = 0x87654321;
+		http_user_data->rc_flash_img = rc;
 	}
 
 	/* Transfer not complete */
-	if ( HTTO_DATA_MORE == data_end )
+	if ( HTTP_DATA_MORE == data_end )
 	{
 		/* Just return, wait for next package */
 		return ;
@@ -71,7 +113,7 @@ static void http_response_cb(struct http_client_ctx *http_ctx,
 	/* Transmision complete */
 
 	/* Check http parse result */
-	if ( HPE_OK != ctx->parser.http_errno )
+	if ( HPE_OK != http_ctx->http.parser.http_errno )
 	{
 		/**
 		 * HTTP format parse error
@@ -81,7 +123,7 @@ static void http_response_cb(struct http_client_ctx *http_ctx,
 		 * (a). const char *http_error_name();
 		 * (b). const char *http_error_description();
 		 * */
-		http_user_data->rc_http_parse = 0x82345678;
+		http_user_data->rc_http_parse = http_ctx->http.parser.http_errno;
 	}
 
 	/**
@@ -91,71 +133,58 @@ static void http_response_cb(struct http_client_ctx *http_ctx,
 	 * ways to check if the firmware valid or not.
 	 * */
 
-	k_sem_give(&http_user_data->sem, 1);
+	k_sem_give(&http_user_data->sem);
 }
 
-static int dfu_get_firmware_via_http(struct http_client_ctx, const char *url)
+static int dfu_get_firmware_via_http(struct http_ctx *http_ctx,
+			const char *host_name, const char *path)
 {
-	static const int http_rx_buff_size	= CONFIG_APP_DFU_HTTP_RX_BUFF_SIZE;
-	static const int http_tx_timeout	= CONFIG_APP_DFU_HTTP_TX_TIMEOUT;
-	static const int http_rx_timeout	= CONFIG_APP_DFU_HTTP_RX_TIMEOUT;
+	int rc = 0;
 
-	struct http_client_request *		http_req		= NULL;
-	uint8_t *							http_rx_buff	= NULL;
-	struct http_response_user_data *	http_user_data	= NULL;
-	struct flash_img_context *			flash_img_ctx	= NULL;
+	struct http_request			http_req = { };
+	uint8_t *					http_rx_buff	= NULL;
+	struct flash_img_context *	flash_img_ctx	= NULL;
+	struct http_user_data		http_user_data;
 
-	/* HTTP Request structure */
-	http_req = k_malloc(sizeof(struct http_client_request));
-	if ( NULL == http_req )
-	{
-		rc = -ENOMEM;
-		goto out;
-	}
-
-	/* HTTP recive buffer */
-	http_rx_buff = k_malloc(http_rx_buff_size);
+	http_rx_buff = k_malloc(CONFIG_APP_DFU_HTTP_RX_BUFF_SIZE);
 	if ( NULL == http_rx_buff )
 	{
 		rc = -ENOMEM;
+		SYS_LOG_ERR("No memory at line: %d", __LINE__);
 		goto out;
 	}
 
-	/* HTTP user data of response callback */
-	http_user_data = k_malloc(sizeof(struct http_response_user_data));
-	if ( NULL == http_user_data )
-	{
-		rc = -ENOMEM;
-		goto out;
-	}
-
-	/* Flash context write firmware into Slot1 */
-	flash_img_ctx = k_malloc(struct flash_img_context);
+	flash_img_ctx = k_malloc(sizeof(struct flash_img_context));
 	if ( NULL == flash_img_ctx )
 	{
 		rc = -ENOMEM;
+		SYS_LOG_ERR("No memory at line: %d", __LINE__);
 		goto out;
 	}
-	flash_img_init(flash_img_ctx, device_get_binding(CONFIG_FLASH_STM32_DEV_NAME));
 
-	/* Initialize the user data */
-	http_user_data->rc_http_parse	= 0;
-	http_user_data->rc_flash_img	= 0;
-	k_sem_init(http_user_data.sem, 0, 1);
+	/* HTTP Request structure */
+	http_req.method		= HTTP_GET;
+	http_req.url		= path;
+	http_req.protocol	= " " HTTP_PROTOCOL;
+	http_req.host		= host_name;
 
-	/* Initialize the HTTP request structure */
-	memset(&http_req, 0, sizeof(struct http_client_request));
-	http_req->method	= HTTP_GET;
-	http_req->url		= url;
-	http_req->protocol	= " " HTTP_PROTOCOL HTTP_CRLF;
+	/* Flash context write firmware into Slot1 */
+	flash_img_init(flash_img_ctx, device_get_binding(CONFIG_SOC_FLASH_STM32_DEV_NAME));
+
+	/* HTTP user data of response callback */
+	http_user_data.flash_img_ctx	= flash_img_ctx;
+	http_user_data.rc_flash_img		= 0;
+	http_user_data.rc_http_parse	= 0;
+	k_sem_init(&http_user_data.sem, 0, 1);
 
 	/* Get connect to the server */
-	rc = http_client_send_req(http_ctx, http_req, http_response_cb,
-			http_rx_buff, http_rx_buff_size,
-			http_user_date, K_SECONDS(http_tx_timeout));
+	rc = http_client_send_req(http_ctx, &http_req, http_response_cb,
+			http_rx_buff, CONFIG_APP_DFU_HTTP_RX_BUFF_SIZE,
+			&http_user_data, K_SECONDS(CONFIG_APP_DFU_HTTP_TX_TIMEOUT_IN_SEC));
 	if ( rc < 0 && rc != -EINPROGRESS )
 	{
-		rc = 0x12345678;
+		SYS_LOG_ERR("Can not send request, rc = %d, host_name = %s, path = %s",
+				rc, host_name, path);
 		goto out;
 	}
 
@@ -164,75 +193,73 @@ static int dfu_get_firmware_via_http(struct http_client_ctx, const char *url)
 	 * In case of irq called by accident,
 	 * use two times rx timeout as real timeout param.
 	 * */
-	if ( 0 == k_sem_take(&user_data->sem, K_SECONDS(http_rx_timeout)) )
+	rc = k_sem_take(&http_user_data.sem,
+			K_SECONDS(CONFIG_APP_DFU_HTTP_RX_TIMEOUT_IN_SEC));
+	if ( 0 != rc )
 	{
-		/* Wait http response timeout, or never wait */
-		rc = 0x12345678;
+		SYS_LOG_ERR("Wait http transmition complete error, rc = %d", rc);
+		goto out;
 	}
 
-	/* HTTP transmission complete, or some error happened */
-	http_client_release(http_ctx);
-
 out:
-	k_free(user_data);
-	k_free(http_rx_buff);
-	k_free(http_req);
+	/* HTTP transmission complete, or some error happened */
+	http_release(http_ctx);
+
 	k_free(flash_img_ctx);
+	k_free(http_rx_buff);
 
 	return rc;
 }
 
-int dfu_parse_url(const char *url_str, size_t url_len)
+/**
+ * @brief Download firmware via HTTP
+ *
+ * This function will download the firmware using the @uri that caller give.
+ * Firmware will automaticly write into image-slot1 and wait to be checked
+ * and upgrade from it.
+ *
+ * @param uri The dowmload url of firmware
+ *
+ * @param uri_len The length of the download url
+ *
+ * @return	  0 Download success
+ *			< 0 Some error happened, may write flash error
+ * */
+int dfu_http_download(const char *uri, size_t uri_len)
 {
-	int rc;
-	http_parser_url_init(http_url);
-	rc = http_parser_parse_url(url_str, url_len, 0, http_url);
-	if ( 0 != rc )
-	{
-		return rc;
-	}
-	if ( !( http_url->field_set & (1 << UF_HOST) ) ||
-		 !( http_url->field_set & (1 << UF_PATH) ))
-	{
-		return -1;
-	}
-	/**
-	 * TODO
-	 * Parse the schema, use http or https. (Here we default use http)
-	 * Parse the host, use ipv4, ipv6 or domain ? (Here we default use ipv4 address)
-	 * Recheck port value, is the port value useful ? use port 80, 443 ?
-	 * */
-	return 0;
-}
+	int rc = 0;
 
-int dfu_download_firmware(const char *uri, size_t uri_len)
-{
-	static const uint16_t expected_field_set =
-		(1<<UF_SCHEMA) | (1<<UF_HOST) | (1<<UF_PATH);
+	char * host_name		= NULL;
+	size_t host_name_len	= 0;
 
-	int rc;
+	char * path				= NULL;
+	size_t path_len			= 0;
 
-	struct http_parser_url *http_url = NULL;
+	size_t port				= 80;
 
-	char * filed		= NULL;
-	char * field_off	= NULL;
-	size_t field_len	= 0;
+	struct http_parser_url	http_url;
+	struct http_ctx *		http_ctx = NULL;
 
-	/* Alloc uri parser result in heap memory */
-	http_url = k_malloc(sizeof(struct http_parser_url));
-	if ( NULL == http_url )
+	const uint16_t expected_field_set =
+		(1 << UF_SCHEMA) | (1 << UF_HOST) | (1 << UF_PATH);
+
+	http_ctx = k_malloc(sizeof(struct http_ctx));
+	if ( NULL == http_ctx )
 	{
 		rc = -ENOMEM;
+		SYS_LOG_ERR("No memory at line: %d", __LINE__);
 		goto out;
 	}
 
-	http_parser_url_init(http_url);
+	/* Initial http url parser */
+	http_parser_url_init(&http_url);
 
 	/* Parse the uri */
-	rc = http_parser_parse_url(uri_str, uri_len, 0, http_url);
+	rc = http_parser_parse_url(uri, uri_len, 0, &http_url);
 	if ( 0 != rc )
 	{
-		rc = 0x0001111;
+		rc = -EINVAL;
+		SYS_LOG_ERR("Invalid url, http_parser_parse_url return %d", rc);
 		goto out;
 	}
 
@@ -241,36 +268,94 @@ int dfu_download_firmware(const char *uri, size_t uri_len)
 	 * Schema, host and path.
 	 * http://hostname/path
 	 * */
-	if ( expected_field_set != ( http_url->field_set & expected_field_set ) )
+	if ( expected_field_set != ( http_url.field_set & expected_field_set ) )
 	{
-		rc = 0x34234;
+		rc = -EINVAL;
+		SYS_LOG_ERR("http_url->field_set = %d", http_url.field_set);
 		goto out;
 	}
 
-	if (  )
-
-	field_off = http_url->field_data[UF_HOST].off;
-	field_len = http_url->field_data[UF_HOST].len;
-
-	http_ctx = k_malloc(sizeof(struct http_client_ctx));
-
-	const char *host_str = http_url->field_date[UF_HOST].off;
-	size_t host_len = http_url->field_date[UF_HOST].len;
-	host_str[host_len] = '\0';
-
 	/**
-	 * FIXME We use port 80 for default, waht if the url spec a port ?
+	 * TODO Check if we need support https or IPv6 address.
 	 * */
-	rc = http_client_init(http_ctx, host_str, 80);
+
+	/* Host name */
+	host_name_len = http_url.field_data[UF_HOST].len;
+	host_name = k_malloc(host_name_len + 1);
+	if ( NULL == host_name )
+	{
+		rc = -ENOMEM;
+		SYS_LOG_ERR("No memory at line: %d", __LINE__);
+		goto out;
+	}
+
+	/* FIXME IPv6 have a "[" at the start of address and "]" at the end of address */
+	memcpy(host_name, &uri[ http_url.field_data[UF_HOST].off ], host_name_len);
+	host_name[host_name_len] = '\0';
+
+	/* Port */
+	if ( 0 != http_url.port )
+	{
+		port = http_url.port;
+	}
+	else
+	{
+		/* Default use 80, no https support yet */
+		port = 80;
+	}
+
+	/* Start to handle http related ops */
+
+	/* Start to connect */
+	rc = http_client_init(http_ctx, host_name, port, NULL,
+			K_SECONDS(CONFIG_APP_DFU_HTTP_CONNECTION_TIMEOUT_IN_SEC));
 	if ( 0 != rc )
 	{
-		k_free(http_url);
-		k_free(http_ctx);
-		return -1;
+		SYS_LOG_ERR("http_client_init error, rc = %d", rc);
+		goto out;
 	}
-	ota_download_firmware(http_url);
-	return 0;
+
+#if defined(CONFIG_NET_CONTEXT_NET_PKT_POOL)
+	net_app_set_net_pkt_pool(&http_ctx->app_ctx, app_tx_slab, app_data_pool);
+#endif /* CONFIG_NET_CONTEXT_NET_PKT_POOL */
+
+	/* Path name */
+	path_len = http_url.field_data[UF_PATH].len;
+	path = k_malloc(path_len + 1);
+	if ( NULL == path )
+	{
+		rc = -ENOMEM;
+		SYS_LOG_ERR("No memory at line: %d", __LINE__);
+		goto out;
+	}
+
+	memcpy(path, &uri[ http_url.field_data[UF_PATH].off ], path_len);
+	path[path_len] = '\0';
+
+	/* Send request and wait it finish */
+	rc = dfu_get_firmware_via_http(http_ctx, host_name, path);
+
 out:
-	free(http_url);
+	k_free(path);
+	k_free(host_name);
+	k_free(http_ctx);
+
+	return rc;
 }
+
+#ifdef CONFIG_APP_DFU_HTTP_DEBUG
+
+void dfu_debug(void)
+{
+	char uri[] = "http://192.168.0.181/index.html";
+
+	dfu_http_download(uri, sizeof(uri) - 1);
+	while ( 1 )
+	{
+		k_sleep(1000);
+	}
+	return ;
+}
+
+#endif /* CONFIG_APP_DFU_HTTP_DEBUG */
 
--- samples/net/vrbox/src/dfu_http.h
+++ samples/net/vrbox/src/dfu_http.h
@@ -1,3 +1,15 @@
+/**
+ * Copyright (c) 2017 Shenzhen Trylong Intelligence Technology Co., Ltd. All rights reserved.
+ *
+ * @file dfu_http.h
+ *
+ * @brief DFU related API
+ *
+ * @author Ding Tao <miyatsu@qq.com>
+ *
+ * @date 15:45:22 December 6, 2017 GTM+8
+ *
+ * */
 #ifndef DFU_HTTP_H
 #define DFU_HTTP_H
 
@@ -5,7 +17,21 @@
 extern "C" {
 #endif /* __cplusplus */
 
-
+/**
+ * @brief Download firmware via HTTP
+ *
+ * This function will download the firmware using the @uri that caller give.
+ * Firmware will automaticly write into image-slot1 and wait to be checked
+ * and upgrede from it.
+ *
+ * @param uri The download url of firmware
+ *
+ * @param uri_len The length of the download url
+ *
+ * @return	 0 Download success
+ *		   < 0 Some error happened, may write flash error
+ * */
+int dfu_http_download(const char *uri, size_t uri_len);
 
 #ifdef __cplusplus
 }
