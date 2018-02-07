eac468822742e41ccd2898bf09eaa0618dcdd712
Ding Tao <miyatsu@qq.com>
2018-02-07 17:06:10 +0800

    app: vrbox: config: Reorganize CONFIG_* options
    Move all debug related CONFIG_* to one place, so user can enable them by
    one config: CONFIG_APP_DEBUG.
    
    Signed-off-by: Ding Tao <miyatsu@qq.com>

--- samples/net/vrbox/src/config.h
+++ samples/net/vrbox/src/config.h
@@ -9,23 +9,25 @@
 
 #include "version.h"
 
-/* DFU config */
+/* DEBUG config */
 /*****************************************************/
-//#define CONFIG_APP_DFU_HTTP_DEBUG
+#define CONFIG_APP_DEBUG
 
-#define CONFIG_APP_DFU_HTTP_CONNECTION_TIMEOUT_IN_SEC	20
-#define CONFIG_APP_DFU_HTTP_RX_TIMEOUT_IN_SEC			5
-#define CONFIG_APP_DFU_HTTP_TX_TIMEOUT_IN_SEC			5
-
-#define CONFIG_APP_DFU_HTTP_RX_BUFF_SIZE				1024
+#ifdef CONFIG_APP_DEBUG
+#define CONFIG_APP_DFU_HTTP_DEBUG
+#define CONFIG_APP_LOG_HOOK_DEBUG
+#define CONFIG_APP_AXLE_DEBUG
+#define CONFIG_APP_DOOR_DEBUG
+#define CONFIG_APP_INFRARED_DEBUG
+#define CONFIG_APP_HEADSET_DEBUG
+#define CONFIG_APP_SERVICE_DEBUG
+#define CONFIG_APP_MAIN_DEBUG
+#define CONFIG_APP_MQTT_DEBUG
+#endif /* CONFIG_APP_DEBUG */
 /*****************************************************/
-
-/* LOG config */
-//#define CONFIG_APP_LOG_HOOK_DEBUG
-#define CONFIG_APP_LOG_HOOK_DISPATCH_THREAD_STACK_SIZE	2048
-#define CONFIG_APP_LOG_HOOK_LOG_FILE_NAME				"/log.log"
 
 /* Factory test config */
+/*****************************************************/
 #define CONFIG_APP_FACTORY_TEST
 
 #ifdef CONFIG_APP_FACTORY_TEST
@@ -34,40 +36,48 @@
 #define CONFIG_APP_INFRARED_FACTORY_TEST
 #define CONFIG_APP_HEADSET_FACTORY_TEST
 #endif /* CONFIG_APP_FACTORY_TEST */
+/*****************************************************/
+
+/* DFU config */
+/*****************************************************/
+#define CONFIG_APP_DFU_HTTP_CONNECTION_TIMEOUT_IN_SEC	20
+#define CONFIG_APP_DFU_HTTP_RX_TIMEOUT_IN_SEC			5
+#define CONFIG_APP_DFU_HTTP_TX_TIMEOUT_IN_SEC			5
+
+#define CONFIG_APP_DFU_HTTP_RX_BUFF_SIZE				1024
+/*****************************************************/
+
+/* LOG config */
+#define CONFIG_APP_LOG_HOOK_DISPATCH_THREAD_STACK_SIZE	2048
+#define CONFIG_APP_LOG_HOOK_LOG_FILE_NAME				"/log.log"
+
 
 /* Axle config */
-//#define CONFIG_APP_AXLE_DEBUG
 #define CONFIG_APP_AXLE_PWM_DEV_NAME			CONFIG_PWM_STM32_2_DEV_NAME
 #define CONFIG_APP_AXLE_PWM_OUTPUT_CHANNEL		2
 #define CONFIG_APP_AXLE_PWM_PERIOD				5000
 #define CONFIG_APP_AXLE_ROTATE_TIMEOUT_IN_SEC	10
 
 /* Door config */
-//#define CONFIG_APP_DOOR_DEBUG
 #define CONFIG_APP_DOOR_INIT_THREAD_STACK_SIZE	1024
 #define CONFIG_APP_DOOR_INIT_TIMEOUT_IN_SEC		CONFIG_APP_DOOR_CLOSE_TIMEOUT_IN_SEC
 #define CONFIG_APP_DOOR_OPEN_TIMEOUT_IN_SEC		30
 #define CONFIG_APP_DOOR_CLOSE_TIMEOUT_IN_SEC	30
 
-/* Infrared debug */
-//#define CONFIG_APP_INFRARED_DEBUG
+/* Infrared config */
 
 /* Headset config */
-//#define CONFIG_APP_HEADSET_DEBUG
 #define CONFIG_APP_HEADSET_ROTATE_TIMEOUT_IN_SEC			5
 #define CONFIG_APP_HEADSET_HANDSPIKE_PUSH_WAIT_TIME_IN_SEC	2
 #define CONFIG_APP_HEADSET_HANDSPIKE_PULL_WAIT_TIME_IN_SEC	2
 #define CONFIG_APP_HEADSET_CELL_DIFF						3
 
 /* Service config */
-//#define CONFIG_APP_SERVICE_DEBUG
 
 /* Main function config */
-//#define CONFIG_APP_MAIN_DEBUG
 
 /* MQTT config */
 /*****************************************************/
-//#define CONFIG_APP_MQTT_DEBUG
 #define CONFIG_APP_MQTT_INIT_TIMEOUT	( 10 * 1000 )
 #define CONFIG_APP_MQTT_TIMEOUT			( 10 * 1000 )
 
