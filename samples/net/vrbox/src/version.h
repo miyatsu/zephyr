f80a98cc82c18ff90b81d69946539ffe70d9224f
Ding Tao <miyatsu@qq.com>
2018-02-10 15:42:40 +0800

    app: vrbox: Update version to v0.0.2-15
    Change log:
    1.Fix possible unexpected door auto stop when open/close all four doors.
    
    Signed-off-by: Ding Tao <miyatsu@qq.com>

--- samples/net/vrbox/src/version.h
+++ samples/net/vrbox/src/version.h
@@ -21,7 +21,7 @@ extern "C" {
 #define CONFIG_APP_DFU_VERSION_MINOR	0
 #define CONFIG_APP_DFU_VERSION_PATCH	2
 
-#define CONFIG_APP_DFU_VERSION_BUILD	14
+#define CONFIG_APP_DFU_VERSION_BUILD	15
 /**
  * Application version build level,
  * zero means this version is release for production usage,
